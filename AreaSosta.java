5bdfc3e8ce1c88c765a57b493138b8e1f7383f6a
Luca <157226323+NoLuca@users.noreply.github.com>
2025-05-03 16:21:09 +0200

    Impostazione base classi AreaSosta e Hangar

--- AreaSosta.java
+++ AreaSosta.java
@@ -1,3 +1,19 @@
+import java.util.concurrent.Semaphore;
+
 public class AreaSosta {
+    private Semaphore posti = new Semaphore(3);
+
+    public AreaSosta() {
+        
+    }
     
+    public void entra(Aereo aereo) throws InterruptedException{
+        posti.acquire();
+        System.out.println("Aereo " + aereo.getId() + " entra nell'area di sosta");
+    }
+
+    public void esci(Aereo aereo){
+        posti.release();
+        System.out.println("Aereo " + aereo.getId() + " esce dall'area di sosta");
+    }
 }
--- Hangar.java
+++ Hangar.java
@@ -1,3 +1,19 @@
+import java.util.concurrent.Semaphore;
+
 public class Hangar {
+    private Semaphore posti = new Semaphore(5);
+
+    public Hangar() {
+        
+    }
+
+    public void entra(Aereo aereo) throws InterruptedException{
+        posti.acquire();
+        System.out.println("Aereo " + aereo.getId() + " entra nell'hangar");
+    }
     
+    public void esci(Aereo aereo){
+        posti.release();
+        System.out.println("Aereo " + aereo.getId() + " esce dall'hangar");
+    }
 }
