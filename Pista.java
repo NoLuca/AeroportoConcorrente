1635260a0d6492e77ad72c1669805feaebbea570
Luca <157226323+NoLuca@users.noreply.github.com>
2025-05-04 19:08:55 +0200

    Aggiunto un ID alla pista

--- Pista.java
+++ Pista.java
@@ -1,19 +1,32 @@
 import java.util.concurrent.Semaphore;
 
 public class Pista {
+    private int id;
     private Semaphore mutex = new Semaphore(1);
 
-    public Pista() {
-    
+    public Pista(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void occupa(Aereo aereo) throws InterruptedException{
         mutex.acquire();
-        System.out.println("Aereo " + aereo.getId() + " sta occupando la pista X");
+        System.out.println("Aereo " + aereo.getId() + " sta occupando la pista " + getId());
     }
 
     public void libera(Aereo aereo){
         mutex.release();
-        System.out.println("Aereo " + aereo.getId() + " ha liberato la pista X");
+        System.out.println("Aereo " + aereo.getId() + " ha liberato la pista " + getId());
+    }
+
+    public boolean tryOccupa(){
+        return mutex.tryAcquire();
     }
 }
